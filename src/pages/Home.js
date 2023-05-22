c2e55a68926bc5059c3c0444e6cdd260d2b14e22
walyson <walysonmoises1342@gmail.com>
2023-05-21 22:15:42 -0300

    mudado layout da pagina inicial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import SlideA from "../imagens/SlideA.jpg";
+import SlideB from "../imagens/slideb.jpg";
+import SlideC from "../imagens/slideC.jpg";
+import SlideD from "../imagens/SlideD.jpg"
 import "./Home.css"
-import MostView from '../components/MostView';
 
 const Home = () => {
   return (
@@ -9,9 +11,21 @@ const Home = () => {
         
         <div className="slides">
         <div className="headName">WALYSON</div>
-            <img src={SlideA} alt="imagem roupa e tenis" />
-            <h2>Mais Vistos</h2>
-            <MostView/>
+            <div className="imagemprincipal">
+            <img src={SlideB} alt="imagem roupa e tenis"  className="slideb"/>
+            </div>
+            <div className="bywaly">
+              <p>by</p>
+              <h4>Walyson</h4>
+            </div>
+            <div className="imagesHomepage">
+            <img src={SlideA} alt=""  className='SlideA' />
+            <div className="slidesrigth">
+            <img src={SlideC} alt="" className="SlideC" />
+            <img src={SlideD} alt="" className="slideD" />
+            </div>
+            </div>
+
         </div>
     </div>
   )
