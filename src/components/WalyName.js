7f2fc67abf9d839f2a2bd5d48caba81699befa63
walyson <walysonmoises1342@gmail.com>
2023-06-04 13:49:16 -0300

    responsividade

--- src/components/WalyName.js
+++ src/components/WalyName.js
@@ -4,7 +4,11 @@ import styles from "./WalyName.module.css"
 const WalyName = () => {
   return (
     <>
-       <div className={styles.headNameprod}><h2>WALYSON</h2> <p>Desenvolvimento de Interfaces Web - Sistemas de Informação - Campus Betim - PBE - Noite - G1/T2 - 2023/1</p><p>Matricula: 790291</p></div>
+       <div className={styles.headName}>
+          <h4>WALYSON</h4>
+          <p>Desenvolvimento de Interfaces Web - Sistemas de Informação - Campus Betim - PBE - Noite - G1/T2 - 2023/1</p>
+          <p>Matricula: 790291</p>
+        </div>
     </>
   )
 }
